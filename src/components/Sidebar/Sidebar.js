e72e8d038a2e4eb6fd5b2d2e2a7717456771b67a
will2dev <wylliam.bastiani@gmail.com>
2018-12-11 22:56:49 -0200

    checkout

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -10,26 +10,6 @@ function Sidebar ({ history }) {
         type={IconMenuItemType.Action}
         onClick={() => { history.push('/') }} />
 
-      {/* <IconMenuItem 
-        icon="fab fa-dev" 
-        type={IconMenuItemType.Action} /> */}
-
-      {/* <IconMenuItem 
-        icon="fas fa-exclamation-triangle" 
-        type={IconMenuItemType.Action} /> */}
-
-      {/* <IconMenuItem 
-        icon="fab fa-product-hunt" 
-        type={IconMenuItemType.Action} /> */}
-
-      {/* <IconMenuItem 
-        icon="fas fa-skull-crossbones" 
-        type={IconMenuItemType.Action} /> */}
-
-      {/* <IconMenuItem 
-        icon="fas fa-server" 
-        type={IconMenuItemType.Action} /> */}
-
       <IconMenuItem 
         icon="fas fa-plus" 
         type={IconMenuItemType.Action} 
